05dbcb2dba4161309bc312a6058b2a96153275d2
Robson Tenorio <robson.tenorio@gmail.com>
2022-05-19 11:33:05 -0300

    Implementação das tags {{ms}}, {{procol}} e {{hora}}

--- backend/src/helpers/Mustache.ts
+++ backend/src/helpers/Mustache.ts
@@ -2,8 +2,34 @@ import Mustache from "mustache";
 import Contact from "../models/Contact";
 
 export default (body: string, contact: Contact): string => {
+  
+  let ms = "";
+
+  const Hr = new Date();
+
+  const dd: string = ("0" + Hr.getDate()).slice(-2);
+  const mm: string = ("0" + (Hr.getMonth() + 1)).slice(-2);
+  const yy: string = Hr.getFullYear().toString();
+  const hh: number = Hr.getHours();
+  const min: string = ("0" + Hr.getMinutes()).slice(-2);
+  const ss: string = ("0" + Hr.getSeconds()).slice(-2);
+
+  if (hh >= 6){ms = "Bom dia";}
+  if (hh > 11){ms = "Boa tarde";}
+  if (hh > 17){ms = "Boa noite";}
+  if (hh > 23 || hh < 6){ms = "Boa madrugada";}
+
+  let protocol = yy+mm+dd+String(hh)+min+ss;
+  
+  let hora = hh+":"+min+":"+ss;
+
   const view = {
-    name: contact ? contact.name : ""
+    name: contact ? contact.name : "",
+    ms: ms,
+    protocol: protocol,
+    hora: hora,
   };
+
   return Mustache.render(body, view);
+
 };
