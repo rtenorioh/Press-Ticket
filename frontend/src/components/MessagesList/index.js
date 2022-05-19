fd3409824bb22b2e57aaaa3f42eeaa84185ddc47
Robson Tenorio <robson.tenorio@gmail.com>
2022-05-19 20:30:09 -0300

    Alterar da cor de confirmação de leitura

--- frontend/src/components/MessagesList/index.js
+++ frontend/src/components/MessagesList/index.js
@@ -4,7 +4,7 @@ import { isSameDay, parseISO, format } from "date-fns";
 import openSocket from "../../services/socket-io";
 import clsx from "clsx";
 
-import { green } from "@material-ui/core/colors";
+import { blue } from "@material-ui/core/colors";
 import {
   Button,
   CircularProgress,
@@ -54,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
   },
 
   circleLoading: {
-    color: green[500],
+    color: blue[500],
     position: "absolute",
     opacity: "70%",
     top: 0,
@@ -245,7 +245,7 @@ const useStyles = makeStyles((theme) => ({
   },
 
   ackDoneAllIcon: {
-    color: green[500],
+    color: blue[500],
     fontSize: 18,
     verticalAlign: "middle",
     marginLeft: 4,
