7c014d09ff9669121b14fbb0389218f12b355f89
YosefAdPro <yosef@adpronet.com>
2022-06-01 15:31:01 +0300

    Update 20200904070004-create-default-users.ts

--- backend/src/database/seeds/20200904070004-create-default-users.ts
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -6,8 +6,8 @@ module.exports = {
       "Users",
       [
         {
-          name: "PyPress",
-          email: "admin@pypress.com.br",
+          name: "AdPRo",
+          email: "admin@adpro.me",
           passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
           profile: "admin",
           tokenVersion: 0,
