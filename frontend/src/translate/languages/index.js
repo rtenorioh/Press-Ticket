5004ae2250473f271d54965f5e65181c01631700
YosefAdPro <yosef@adpronet.com>
2022-06-01 15:08:55 +0300

    Add hebrew

--- frontend/src/translate/languages/index.js
+++ frontend/src/translate/languages/index.js
@@ -1,11 +1,13 @@
 import { messages as portugueseMessages } from "./pt";
 import { messages as englishMessages } from "./en";
 import { messages as spanishMessages } from "./es";
+import { messages as hebrewMessages } from "./he";
 
 const messages = {
 	...portugueseMessages,
 	...englishMessages,
 	...spanishMessages,
+	...hebrewMessages,
 };
 
 export { messages };
