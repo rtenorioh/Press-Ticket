ea9c9a9b6b5bfcf5a0bf0f8e14d21f8b94b89149
YosefAdPro <yosef@adpronet.com>
2022-06-01 15:27:40 +0300

    Update index.js

--- frontend/src/pages/Api/index.js
+++ frontend/src/pages/Api/index.js
@@ -47,40 +47,40 @@ const Api = () => {
     return (
         <div className={classes.root}>
             <Container>
-                <h2>Documentação para envio de mensagens</h2>
+                <h2>תיעוד לשליחת הודעות</h2>
 
-                <h2 className={classes.color}>Métodos de Envio</h2>
+                <h2 className={classes.color}>שיטות משלוח</h2>
 
-                <p className={classes.text}>1. Mensagens de Texto</p>
-                <p className={classes.text}>2. Mensagens de Mídia</p>
+                <p className={classes.text}>1. הודעות טקסט</p>
+                <p className={classes.text}>2. הודעות מדיה</p>
 
-                <h2 className={classes.color}>Instruções</h2>
-                <p><b>Observações Importantes</b></p>
+                <h2 className={classes.color}>הוראות</h2>
+                <p><b>הערות חשובות</b></p>
                 <ul>
-                    <li className={classes.text}>Para pegar o token da API, vá em configurações que seu token estará la, sem ele não será possivel enviar mensagens.</li>
-                    <li className={classes.text}>O número para envio não deve ter mascara ou caracteres especiais e deve ser composto por:</li>
+                    <li className={classes.text}>כדי לקבל את אסימון ה-API, היכנסו להגדרות והטוקן שלכם יהיה שם, בלעדיו לא ניתן יהיה לשלוח הודעות..</li>
+                    <li className={classes.text}>למספר השולח אסור לכלול מסכה או תווים מיוחדים והוא חייב להיות מורכב מ:</li>
                     <br />
                     <ol>
                         <ul>
-                            <li className={classes.text}>Código do pais - Ex: 55 (Brasil)</li>
+                            <li className={classes.text}>קידומת מדינה - לדוגמה: 972 (ישראל)</li>
                             <li className={classes.text}>DDD</li>
-                            <li className={classes.text}>Número</li>
+                            <li className={classes.text}>מספר</li>
                         </ul>
                     </ol>
                 </ul>
-                <h2 className={classes.color}>1. Mensagens de Texto</h2>
-                <p>Seguem abaixo lista de informações necessárias para envio das mensagens de texto:</p>
-                <p className={classes.textP}><b>URL: </b>{process.env.REACT_APP_BACKEND_URL}/api/messages/send</p>
-                <p className={classes.textP}><b>Metódo: </b>POST</p>
-                <p className={classes.textP}><b>Headers: </b>Authorization: Bearer (token) e Content-Type application/json</p>
-                <p className={classes.textP}><b>Body: </b>"number": "5599999999999", "body": "Enviado via api"</p>
+                <h2 className={classes.color}>1. הודעות טקסט</h2>
+                <p>להלן רשימה של מידע הדרוש לשליחת הודעות טקסט:</p>
+                <p className={classes.textP}><b>כתובת אתר: </b>{process.env.REACT_APP_BACKEND_URL}/api/messages/send</p>
+                <p className={classes.textP}><b>שיטה: </b>POST</p>
+                <p className={classes.textP}><b>כותרות: </b>Authorization: Bearer (token) e Content-Type application/json</p>
+                <p className={classes.textP}><b>Body: </b>"number": "972540000000", "body": "Test via api"</p>
 
-                <h2 className={classes.color}>2. Mensagens de Mídia</h2>
-                <p>Seguem abaixo lista de informações necessárias para envio de midias:</p>
-                <p className={classes.textP}><b>URL: </b>{process.env.REACT_APP_BACKEND_URL}/api/messages/send</p>
-                <p className={classes.textP}><b>Metódo: </b>POST</p>
-                <p className={classes.textP}><b>Headers: </b>Authorization: Bearer (token) e Content-Type multipart/form-data</p>
-                <p className={classes.textP}><b>Body: </b>"number": "5599999999999", "medias": "aqui vai sua midia", "body": "Enviado via api"</p>
+                <h2 className={classes.color}>2. הודעות מדיה</h2>
+                <p>להלן רשימה של מידע הדרוש לשליחת מדיה:</p>
+                <p className={classes.textP}><b>כתובת אתר: </b>{process.env.REACT_APP_BACKEND_URL}/api/messages/send</p>
+                <p className={classes.textP}><b>שיטה: </b>POST</p>
+                <p className={classes.textP}><b>כותרות: </b>Authorization: Bearer (token) e Content-Type multipart/form-data</p>
+                <p className={classes.textP}><b>Body: </b>"number": "9725400000000", "media": "here goes your media", "body": "Sent via api"</p>
             </Container>
         </div>
     );
