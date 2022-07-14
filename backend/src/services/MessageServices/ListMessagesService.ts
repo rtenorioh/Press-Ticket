45f06259d72c8546547d2696c3131a6720486e9a
Wanderlei Rodrigues <wanderlei.safira@gmail.com>
2022-07-14 11:05:19 -0300

    Update ListMessagesService.ts
    retirado dependências não usadas

--- backend/src/services/MessageServices/ListMessagesService.ts
+++ backend/src/services/MessageServices/ListMessagesService.ts
@@ -2,7 +2,7 @@ import AppError from "../../errors/AppError";
 import Message from "../../models/Message";
 import Ticket from "../../models/Ticket";
 import ShowTicketService from "../TicketServices/ShowTicketService";
-import { Op, fn, where, col, Filterable, Includeable } from "sequelize";
+import { Op } from "sequelize";
 
 interface Request {
   ticketId: string;
@@ -43,7 +43,7 @@ const ListMessagesService = async ({
       },
       {
         model: Ticket,
-        where: {contactId: ticket.contactId  },
+        where: { contactId: ticket.contactId },
         required: true
       },
       {
@@ -52,7 +52,6 @@ const ListMessagesService = async ({
           whatsappId: ticket.whatsappId,
           queueId: { [Op.or]: [ticket.queueId, null] }
         },
-        //where: { queueId: { [Op.or]: [ticket.queueId, null] } },
         required: true
       }
     ],
