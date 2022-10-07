c3b3c19c59cbccac74f9141abb65a6e5ec41c302
Wesleyjan Lacerda <74305459+WesleyjanLacerda@users.noreply.github.com>
2022-10-07 14:57:29 -0300

    Update index.js

--- frontend/src/components/MainHeaderButtonsWrapper/index.js
+++ frontend/src/components/MainHeaderButtonsWrapper/index.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
 	MainHeaderButtonsWrapper: {
-		flex: "none",
+		display: "flex",
 		marginLeft: "auto",
 		"& > *": {
 			margin: theme.spacing(1),
