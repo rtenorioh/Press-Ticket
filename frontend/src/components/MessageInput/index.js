db8ae4a84945ccaab7a6fe233b9c52054dc9e754
LCarneiroAD <lcarneiro@arquitetodigital.com.br>
2022-10-14 19:27:51 -0300

    Retirada do checkbox Assinatura
    Assinatura passa a ser obrigatória.

--- frontend/src/components/MessageInput/index.js
+++ frontend/src/components/MessageInput/index.js
@@ -588,22 +588,6 @@ const MessageInput = ({ ticketStatus }) => {
                 <AttachFileIcon className={classes.sendMessageIcons} />
               </IconButton>
             </label>
-            <FormControlLabel
-              style={{ marginRight: 7, color: "gray" }}
-              label={i18n.t("messagesInput.signMessage")}
-              labelPlacement="start"
-              control={
-                <Switch
-                  size="small"
-                  checked={signMessage}
-                  onChange={(e) => {
-                    setSignMessage(e.target.checked);
-                  }}
-                  name="showAllTickets"
-                  color="primary"
-                />
-              }
-            />
           </Hidden>
           <Hidden only={["md", "lg", "xl"]}>
             <IconButton
@@ -649,24 +633,7 @@ const MessageInput = ({ ticketStatus }) => {
                   </IconButton>
                 </label>
               </MenuItem>
-              <MenuItem onClick={handleMenuItemClick}>
-                <FormControlLabel
-                  style={{ marginRight: 7, color: "gray" }}
-                  label={i18n.t("messagesInput.signMessage")}
-                  labelPlacement="start"
-                  control={
-                    <Switch
-                      size="small"
-                      checked={signMessage}
-                      onChange={(e) => {
-                        setSignMessage(e.target.checked);
-                      }}
-                      name="showAllTickets"
-                      color="primary"
-                    />
-                  }
-                />
-              </MenuItem>
+
             </Menu>
           </Hidden>
           <div className={classes.messageInputWrapper}>
