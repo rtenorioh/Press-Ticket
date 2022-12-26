f586f4b35e4d4a906e02b3f4995142c947acf5e4
Filipe Camillo <64233640+FilipeCamillo@users.noreply.github.com>
2022-12-26 11:26:11 -0300

    Enabled sideMenu

--- backend/src/database/seeds/20200904070004-create-default-settings.ts
+++ backend/src/database/seeds/20200904070004-create-default-settings.ts
@@ -25,7 +25,7 @@ module.exports = {
         },
         {
           key: "sideMenu",
-          value: "disabled",
+          value: "enabled",
           createdAt: new Date(),
           updatedAt: new Date()
         },
