406c5ffe7c31a538770d6b1d8b2d836f62a747d3
Robson Tenorio <robson.tenorio@gmail.com>
2023-03-21 14:30:37 -0300

    Atualização de Libs e correção no logger
    Atualizado as libs: pino, pino-pretty e whatsapp-web.js

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -2,11 +2,11 @@ import pino from "pino";
 
 const logger = pino({
   transport: {
-    target: 'pino-pretty',
+    target: "pino-pretty",
     options: {
       levelFirst: true,
       translateTime: true,
-      colorize: true,
+      colorize: true
     }
   }
 });
