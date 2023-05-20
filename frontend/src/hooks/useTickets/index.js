92873cab890219380b0496ae1d8cb22e7c2ecfaa
Filipe Camillo <64233640+FilipeCamillo@users.noreply.github.com>
2023-05-20 09:48:50 -0300

    update frontend/hooks/useTickets

--- frontend/src/hooks/useTickets/index.js
+++ frontend/src/hooks/useTickets/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getHoursCloseTicketsAuto } from "../../config";
+// import { getHoursCloseTicketsAuto } from "../../config";
 import toastError from "../../errors/toastError";
 
 import api from "../../services/api";
@@ -38,22 +38,22 @@ const useTickets = ({
                     })
                     setTickets(data.tickets)
 
-                    let horasFecharAutomaticamente = getHoursCloseTicketsAuto(); 
+                    // let horasFecharAutomaticamente = getHoursCloseTicketsAuto(); 
 
-                    if (status === "open" && horasFecharAutomaticamente && horasFecharAutomaticamente !== "" &&
-                        horasFecharAutomaticamente !== "0" && Number(horasFecharAutomaticamente) > 0) {
+                    // if (status === "open" && horasFecharAutomaticamente && horasFecharAutomaticamente !== "" &&
+                    //     horasFecharAutomaticamente !== "0" && Number(horasFecharAutomaticamente) > 0) {
 
-                        let dataLimite = new Date()
-                        dataLimite.setHours(dataLimite.getHours() - Number(horasFecharAutomaticamente))
+                    //     let dataLimite = new Date()
+                    //     dataLimite.setHours(dataLimite.getHours() - Number(horasFecharAutomaticamente))
 
-                        data.tickets.forEach(ticket => {
-                            if (ticket.status !== "closed") {
-                                let dataUltimaInteracaoChamado = new Date(ticket.updatedAt)
-                                if (dataUltimaInteracaoChamado < dataLimite)
-                                    closeTicket(ticket)
-                            }
-                        })
-                    }
+                    //     data.tickets.forEach(ticket => {
+                    //         if (ticket.status !== "closed") {
+                    //             let dataUltimaInteracaoChamado = new Date(ticket.updatedAt)
+                    //             if (dataUltimaInteracaoChamado < dataLimite)
+                    //                 closeTicket(ticket)
+                    //         }
+                    //     })
+                    // }
 
                     setHasMore(data.hasMore)
                     setCount(data.count)
