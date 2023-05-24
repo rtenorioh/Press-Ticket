82e360bd42dc277f100cd1fafc09e66e06d30d83
Filipe Camillo <64233640+FilipeCamillo@users.noreply.github.com>
2023-05-24 10:44:35 -0300

    Teste de audio IOS

--- frontend/src/components/Audio/index.jsx
+++ frontend/src/components/Audio/index.jsx
@@ -1,60 +1,92 @@
-import { Button } from "@material-ui/core";
-
 import React, { useRef, useEffect, useState } from "react";
 
 const LS_NAME = 'audioMessageRate';
 
-export default function({url}) {
-    const audioRef = useRef(null);
-    const [audioRate, setAudioRate] = useState( parseFloat(localStorage.getItem(LS_NAME) || "1") );
-    const [showButtonRate, setShowButtonRate] = useState(false);
+export default function AudioPlayer({ url }) {
+  const audioRef = useRef(null);
+  const [audioRate, setAudioRate] = useState(
+    parseFloat(localStorage.getItem(LS_NAME) || "1")
+  );
+  const [showButtonRate, setShowButtonRate] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
 
-    useEffect(() => {
-        audioRef.current.playbackRate = audioRate;
-        localStorage.setItem(LS_NAME, audioRate);
-    }, [audioRate]);
-    useEffect(() => {
-        audioRef.current.onplaying = () => {
-            setShowButtonRate(true);
-        };
-        audioRef.current.onpause = () => {
-            setShowButtonRate(false);
-        };
-        audioRef.current.onended = () => {
-            setShowButtonRate(false);
-        };
-    }, []);
+  useEffect(() => {
+    audioRef.current.playbackRate = audioRate;
+    localStorage.setItem(LS_NAME, audioRate);
+  }, [audioRate]);
 
-    const toogleRate = () => {
-        let newRate = null;
+  useEffect(() => {
+    const isDeviceIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    setIsIOS(isDeviceIOS);
 
-        switch(audioRate) {
-            case 0.5:
-                newRate = 1;
-                break;
-            case 1:
-                newRate = 1.5;
-                break;
-            case 1.5:
-                newRate = 2;
-                break;
-            case 2:
-                newRate = 0.5;
-                break;
-            default:
-                newRate = 1;
-                break;
-        }
+    audioRef.current.addEventListener("play", () => {
+      setShowButtonRate(true);
+    });
+    audioRef.current.addEventListener("pause", () => {
+      setShowButtonRate(false);
+    });
+    audioRef.current.addEventListener("ended", () => {
+      setShowButtonRate(false);
+    });
 
-        setAudioRate(newRate);
+    return () => {
+      audioRef.current.removeEventListener("play", () => {
+        setShowButtonRate(true);
+      });
+      audioRef.current.removeEventListener("pause", () => {
+        setShowButtonRate(false);
+      });
+      audioRef.current.removeEventListener("ended", () => {
+        setShowButtonRate(false);
+      });
     };
+  }, []);
 
-    return (
-        <>
-            <audio ref={audioRef} controls>
-                <source src={url} type="audio/ogg"></source>
-            </audio>
-            {showButtonRate && <Button style={{marginLeft: "5px", marginTop: "-45px"}} onClick={toogleRate}>{audioRate}x</Button>}
-        </>
-    );
+  const toggleRate = () => {
+    let newRate = null;
+
+    switch (audioRate) {
+      case 0.5:
+        newRate = 1;
+        break;
+      case 1:
+        newRate = 1.5;
+        break;
+      case 1.5:
+        newRate = 2;
+        break;
+      case 2:
+        newRate = 0.5;
+        break;
+      default:
+        newRate = 1;
+        break;
+    }
+
+    setAudioRate(newRate);
+  };
+
+  const playAudio = () => {
+    audioRef.current.play();
+  };
+
+  return (
+    <>
+      {isIOS ? (
+        <button onClick={playAudio}>Play Audio</button>
+      ) : (
+        <audio ref={audioRef} controls>
+          <source src={url} type="audio/ogg" />
+        </audio>
+      )}
+      {showButtonRate && (
+        <button
+          style={{ marginLeft: "5px", marginTop: "-45px" }}
+          onClick={toggleRate}
+        >
+          {audioRate}x
+        </button>
+      )}
+    </>
+  );
 }
