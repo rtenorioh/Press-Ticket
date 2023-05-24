ae1843a5fb44991b28c3fcb368bc746be42fbfe6
Filipe Camillo <64233640+FilipeCamillo@users.noreply.github.com>
2023-05-24 16:22:53 -0300

    Normalizando audio

--- frontend/src/components/Audio/index.jsx
+++ frontend/src/components/Audio/index.jsx
@@ -1,86 +1,60 @@
+import { Button } from "@material-ui/core";
+
 import React, { useRef, useEffect, useState } from "react";
-import ReactPlayer from "react-player";
 
 const LS_NAME = 'audioMessageRate';
 
-export default function AudioPlayer({ url }) {
-  const audioRef = useRef(null);
-  const [audioRate, setAudioRate] = useState(
-    parseFloat(localStorage.getItem(LS_NAME) || "1")
-  );
-  const [showButtonRate, setShowButtonRate] = useState(false);
+export default function({url}) {
+    const audioRef = useRef(null);
+    const [audioRate, setAudioRate] = useState( parseFloat(localStorage.getItem(LS_NAME) || "1") );
+    const [showButtonRate, setShowButtonRate] = useState(false);
 
-  useEffect(() => {
-    audioRef.current.seekTo(0);
-    audioRef.current.playbackRate = audioRate;
-    localStorage.setItem(LS_NAME, audioRate);
-  }, [audioRate]);
+    useEffect(() => {
+        audioRef.current.playbackRate = audioRate;
+        localStorage.setItem(LS_NAME, audioRate);
+    }, [audioRate]);
+    useEffect(() => {
+        audioRef.current.onplaying = () => {
+            setShowButtonRate(true);
+        };
+        audioRef.current.onpause = () => {
+            setShowButtonRate(false);
+        };
+        audioRef.current.onended = () => {
+            setShowButtonRate(false);
+        };
+    }, []);
 
-  useEffect(() => {
-    audioRef.current.wrapper.addEventListener("play", () => {
-      setShowButtonRate(true);
-    });
-    audioRef.current.wrapper.addEventListener("pause", () => {
-      setShowButtonRate(false);
-    });
-    audioRef.current.wrapper.addEventListener("ended", () => {
-      setShowButtonRate(false);
-    });
+    const toogleRate = () => {
+        let newRate = null;
 
-    return () => {
-      audioRef.current.wrapper.removeEventListener("play", () => {
-        setShowButtonRate(true);
-      });
-      audioRef.current.wrapper.removeEventListener("pause", () => {
-        setShowButtonRate(false);
-      });
-      audioRef.current.wrapper.removeEventListener("ended", () => {
-        setShowButtonRate(false);
-      });
+        switch(audioRate) {
+            case 0.5:
+                newRate = 1;
+                break;
+            case 1:
+                newRate = 1.5;
+                break;
+            case 1.5:
+                newRate = 2;
+                break;
+            case 2:
+                newRate = 0.5;
+                break;
+            default:
+                newRate = 1;
+                break;
+        }
+
+        setAudioRate(newRate);
     };
-  }, []);
 
-  const toggleRate = () => {
-    let newRate = null;
-
-    switch (audioRate) {
-      case 0.5:
-        newRate = 1;
-        break;
-      case 1:
-        newRate = 1.5;
-        break;
-      case 1.5:
-        newRate = 2;
-        break;
-      case 2:
-        newRate = 0.5;
-        break;
-      default:
-        newRate = 1;
-        break;
-    }
-
-    setAudioRate(newRate);
-  };
-
-  return (
-    <>
-      <ReactPlayer
-        ref={audioRef}
-        url={url}
-        controls
-        width="100%"
-        height="auto"
-      />
-      {showButtonRate && (
-        <button
-          style={{ marginLeft: "5px", marginTop: "-45px" }}
-          onClick={toggleRate}
-        >
-          {audioRate}x
-        </button>
-      )}
-    </>
-  );
+    return (
+        <>
+            <audio ref={audioRef} controls>
+                <source src={url} type="audio/ogg"></source>
+            </audio>
+            {showButtonRate && <Button style={{marginLeft: "5px", marginTop: "-45px"}} onClick={toogleRate}>{audioRate}x</Button>}
+        </>
+    );
 }
