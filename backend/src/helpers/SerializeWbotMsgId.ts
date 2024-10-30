c11e07d13d5f788e8ec1756d8eb7bbd7071e4517
T3uZ <matheusferreira991@gmail.com>
2024-10-30 16:02:32 -0300

    Fix: Conseguir responder mensagem dentro de grupos

--- backend/src/helpers/SerializeWbotMsgId.ts
+++ backend/src/helpers/SerializeWbotMsgId.ts
@@ -2,11 +2,9 @@ import Message from "../models/Message";
 import Ticket from "../models/Ticket";
 
 const SerializeWbotMsgId = (ticket: Ticket, message: Message): string => {
-
-  let SeGrupo = 'g.us_' + message.id + '_'+ message.contact.number + '@c.us';
-  let SeIndiv = 'c.us_' + message.id;
-
-  const serializedMsgId = `${message.fromMe}_${ticket.contact.number}@${ticket.isGroup ? SeGrupo : SeIndiv }`;
+  const serializedMsgId = `${message.fromMe}_${ticket.contact.number}@${
+    ticket.isGroup ? "g" : "c"
+  }.us_${message.id}`;
 
   return serializedMsgId;
 };
