b24ff27216e47c35ab2035d313b63a9b5d11e774
Robson Tenorio Henriques <robson.tenorio@gmail.com>
2024-12-21 00:35:05 -0300

    Add INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,303 @@
+#!/bin/bash
+
+# Função para exibir uso correto do script
+show_usage() {
+    echo "Uso: curl -sSL https://install.pressticket.com.br | sudo bash -s <SENHA_DEPLOY> <NOME_EMPRESA> <URL_BACKEND> <URL_FRONTEND> <PORT_BACKEND> <PORT_FRONTEND> <USER_LIMIT> <CONNECTION_LIMIT> <EMAIL>"
+    echo "Exemplo: curl -sSL https://install.pressticket.com.br | sudo bash -s 'senha123' 'empresa' 'back.pressticket.com.br' 'front.pressticket.com.br' 8080 3333 3 10 'admin@pressticket.com.br'"
+    exit 1
+}
+
+# Verifica se todos os parâmetros foram fornecidos
+if [ "$#" -ne 9 ]; then
+    echo "Erro: Número insuficiente de parâmetros fornecidos."
+    show_usage
+fi
+
+# Recebe os parâmetros
+SENHA_DEPLOY="$1"
+NOME_EMPRESA="$2"
+URL_BACKEND="$3"
+URL_FRONTEND="$4"
+PORT_BACKEND="$5"
+PORT_FRONTEND="$6"
+USER_LIMIT="$7"
+CONNECTION_LIMIT="$8"
+EMAIL="$9"
+
+# Verifica se o script está sendo executado como root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "\e[31mErro: Este script precisa ser executado como root.\e[0m"
+    exit 1
+fi
+
+# Define o diretório base absoluto
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# Define diretórios de logs usando caminhos absolutos
+LOG_DIR="$SCRIPT_DIR/log"
+CURRENT_LOG_DIR="$LOG_DIR/atual"
+ARCHIVED_LOG_DIR="$LOG_DIR/arquivos"
+
+# Cria os diretórios de log
+if ! mkdir -p "$CURRENT_LOG_DIR" "$ARCHIVED_LOG_DIR"; then
+    echo "Erro: Não foi possível criar os diretórios de log. Verifique as permissões."
+    exit 1
+fi
+
+COLOR="\e[38;5;92m"
+GREEN="\e[32m"
+RESET="\e[0m"
+BOLD="\e[1m"
+
+echo -e " "
+echo -e "${COLOR}██████╗ ██████╗ ███████╗███████╗███████╗    ████████╗██╗ ██████╗██╗  ██╗███████╗████████╗${RESET}"
+echo -e "${COLOR}██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝    ╚══██╔══╝██║██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝${RESET}"
+echo -e "${COLOR}██████╔╝██████╔╝█████╗  ███████╗███████╗       ██║   ██║██║     █████╔╝ █████╗     ██║   ${RESET}"
+echo -e "${COLOR}██╔═══╝ ██╔══██╗██╔══╝  ╚════██║╚════██║       ██║   ██║██║     ██╔═██╗ ██╔══╝     ██║   ${RESET}"
+echo -e "${COLOR}██║     ██║  ██║███████╗███████║███████║       ██║   ██║╚██████╗██║  ██╗███████╗   ██║   ${RESET}"
+echo -e "${COLOR}╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝       ╚═╝   ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ${RESET}"
+echo -e "${GREEN}INICIANDO INSTALAÇÃO PARA A EMPRESA:${RESET} ${BOLD}$NOME_EMPRESA${RESET}"
+echo -e " "
+
+sleep 3
+
+# Exibir mensagem com a lista de fusos horários
+echo "O fuso horário padrão está definido como 'America/Sao_Paulo'."
+echo ""
+echo "Caso deseje usar outro fuso horário, utilize o comando abaixo para ver a lista completa:"
+echo ""
+echo "    timedatectl list-timezones"
+echo ""
+echo "Para parar a execução atual e rodar o comando acima, pressione 'CTRL + C'."
+echo "Depois de escolher o fuso horário desejado, execute novamente o script informando o fuso como parâmetro."
+echo ""
+echo "Por exemplo, para usar o fuso horário 'Asia/Kolkata', execute:"
+echo ""
+echo "    curl -sSL https://update.pressticket.com.br | sudo bash -s -- Asia/Kolkata"
+echo ""
+
+# Pausa para o usuário ler a mensagem
+sleep 10
+
+# Compactação de logs antigos usando zip
+if find "$CURRENT_LOG_DIR" -type f -mtime +30 | grep -q .; then
+    zip -j "$ARCHIVED_LOG_DIR/logs_$(date +'%d-%m-%Y').zip" "$CURRENT_LOG_DIR"/* -x "*.zip"
+    if [ $? -eq 0 ]; then
+        echo "Logs antigos compactados com sucesso em $ARCHIVED_LOG_DIR/logs_$(date +'%d-%m-%Y').zip"
+        # Remove os arquivos compactados após o sucesso
+        find "$CURRENT_LOG_DIR" -type f -mtime +30 -exec rm {} \;
+    else
+        echo "Erro ao compactar os logs antigos."
+    fi
+else
+    echo "Nenhum log antigo encontrado para compactar."
+fi
+
+# Captura o fuso horário passado como argumento ou usa America/Sao_Paulo} como padrão
+SELECTED_TZ=${1:-America/Sao_Paulo}
+
+# Configuração do arquivo de log (ajustado para usar o fuso horário)
+LOG_FILE="$CURRENT_LOG_DIR/update_$NOME_EMPRESA_$(TZ=$SELECTED_TZ date +"%d-%m-%Y_%H-%M-%S").log"
+
+# Função para verificar se um comando existe
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Seção 1: Preparação Inicial
+echo -e "${COLOR}Preparação Inicial...${RESET}" | tee -a "$LOG_FILE"
+
+# Atualizando e fazendo upgrade da VPS
+echo "Atualizando pacotes do sistema..." | tee -a "$LOG_FILE"
+sudo apt update && sudo apt upgrade -y | tee -a "$LOG_FILE"
+
+# Seção 2: Instalação do MySQL
+echo -e "${COLOR}Instalando MySQL...${RESET}" | tee -a "$LOG_FILE"
+sudo apt install -y mysql-server | tee -a "$LOG_FILE"
+
+# Configurando banco de dados
+sudo mysql -u root <<MYSQL_SCRIPT
+CREATE DATABASE $NOME_EMPRESA CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE mysql;
+UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+sudo service mysql restart | tee -a "$LOG_FILE"
+
+# Seção 3: Configuração do Usuário
+echo -e "${GREEN}Criando usuário deploy...${RESET}" | tee -a "$LOG_FILE"
+sudo adduser deploy --gecos "" --disabled-password | tee -a "$LOG_FILE"
+echo "deploy:$SENHA_DEPLOY" | sudo chpasswd | tee -a "$LOG_FILE"
+sudo usermod -aG sudo deploy | tee -a "$LOG_FILE"
+
+# Seção 4: Instalação do Node.js e Dependências
+echo -e "${COLOR}Instalando Node.js e dependências...${RESET}" | tee -a "$LOG_FILE"
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs apt-transport-https ca-certificates curl software-properties-common git ffmpeg | tee -a "$LOG_FILE"
+
+# Adicionando o usuário ao grupo mysql
+sudo usermod -aG mysql "${USER}"
+
+# Automatizando o comando su com expect
+if ! command -v expect &>/dev/null; then
+    echo "Instalando a ferramenta 'expect' para automação..." | tee -a "$LOG_FILE"
+    sudo apt-get install -y expect | tee -a "$LOG_FILE"
+fi
+
+echo -e "${COLOR}Automatizando o login como usuário '${USER}'...${RESET}" | tee -a "$LOG_FILE"
+
+expect <<EOF
+spawn su - ${USER}
+expect "Password:"
+send "${SENHA_DEPLOY}\n"
+interact
+EOF
+
+# Seção 5: Instalação do Chrome e Dependências
+echo -e "${COLOR}Instalando Google Chrome e dependências...${RESET}" | tee -a "$LOG_FILE"
+sudo apt-get install -y libgbm-dev wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils | tee -a "$LOG_FILE"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb | tee -a "$LOG_FILE"
+
+# Seção 6: Baixando Press Ticket
+echo -e "${COLOR}Baixando repositório do Press Ticket...${RESET}" | tee -a "$LOG_FILE"
+git clone https://github.com/rtenorioh/Press-Ticket.git $NOME_EMPRESA | tee -a "$LOG_FILE"
+
+# Configurando Backend
+echo -e "${COLOR}Configurando Backend...${RESET}" | tee -a "$LOG_FILE"
+cd $NOME_EMPRESA/backend
+
+cat >.env <<EOF
+NODE_ENV=production
+BACKEND_URL=https://$URL_BACKEND
+FRONTEND_URL=https://$URL_FRONTEND
+WEBHOOK=https://$URL_BACKEND
+PORT=$PORT_BACKEND
+PROXY_PORT=443
+CHROME_BIN=/usr/bin/google-chrome-stable
+DB_DIALECT=mysql
+DB_HOST=localhost
+DB_TIMEZONE=-03:00
+DB_USER=root
+DB_PASS=
+DB_NAME=$NOME_EMPRESA
+USER_LIMIT=$USER_LIMIT
+CONNECTIONS_LIMIT=$CONNECTION_LIMIT
+PM2_FRONTEND=0
+PM2_BACKEND=1
+DEMO=OFF
+JWT_SECRET=$(openssl rand -base64 32)
+JWT_REFRESH_SECRET=$(openssl rand -base64 32)
+EOF
+npm install | tee -a "$LOG_FILE"
+npm run build | tee -a "$LOG_FILE"
+npx sequelize db:migrate | tee -a "$LOG_FILE"
+npx sequelize db:seed:all | tee -a "$LOG_FILE"
+sudo npm install -g pm2 | tee -a "$LOG_FILE"
+pm2 start dist/server.js --name $NOME_EMPRESA-back | tee -a "$LOG_FILE"
+sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u deploy --hp /home/deploy
+
+# Configurando Frontend
+echo -e "${COLOR}Configurando Frontend...${RESET}" | tee -a "$LOG_FILE"
+cd ../frontend
+cat >.env <<EOF
+REACT_APP_BACKEND_URL=https://$URL_BACKEND
+REACT_APP_HOURS_CLOSE_TICKETS_AUTO=
+PORT=$PORT_FRONTEND
+REACT_APP_MASTERADMIN=OFF
+EOF
+npm install | tee -a "$LOG_FILE"
+npm run build | tee -a "$LOG_FILE"
+pm2 start server.js --name $NOME_EMPRESA-front | tee -a "$LOG_FILE"
+pm2 save | tee -a "$LOG_FILE"
+
+# Configurando Nginx
+echo -e "${COLOR}Configurando Nginx...${RESET}" | tee -a "$LOG_FILE"
+sudo apt install nginx -y | tee -a "$LOG_FILE"
+cat >/etc/nginx/sites-available/$NOME_EMPRESA-front <<EOF
+server {
+  server_name $URL_FRONTEND;
+  location / {
+    proxy_pass http://127.0.0.1:$PORT_FRONTEND;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
+  }
+}
+EOF
+
+cat >/etc/nginx/sites-available/$NOME_EMPRESA-back <<EOF
+server {
+  server_name $URL_BACKEND;
+  location / {
+    proxy_pass http://127.0.0.1:$PORT_BACKEND;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
+  }
+}
+EOF
+
+sudo ln -s /etc/nginx/sites-available/$NOME_EMPRESA-front /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/$NOME_EMPRESA-back /etc/nginx/sites-enabled
+sudo nginx -t | tee -a "$LOG_FILE"
+sudo service nginx restart | tee -a "$LOG_FILE"
+
+# Adiciona configuração no nginx.conf
+echo -e "${GREEN}Configurando client_max_body_size no Nginx...${RESET}" | tee -a "$LOG_FILE"
+sudo sed -i '/http {/a \    client_max_body_size 50M;' /etc/nginx/nginx.conf | tee -a "$LOG_FILE"
+
+# Testa e reinicia o Nginx
+sudo nginx -t | tee -a "$LOG_FILE"
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Testes do Nginx bem-sucedidos! Reiniciando...${RESET}" | tee -a "$LOG_FILE"
+    sudo service nginx restart | tee -a "$LOG_FILE"
+else
+    echo -e "${RED}Erro na configuração do Nginx. Verifique os logs.${RESET}" | tee -a "$LOG_FILE"
+    exit 1
+fi
+
+# Instalação de Certificado SSL
+echo -e "${COLOR}Instalando Certificado SSL...${RESET}" | tee -a "$LOG_FILE"
+sudo apt-get install snapd -y | tee -a "$LOG_FILE"
+sudo snap install --classic certbot | tee -a "$LOG_FILE"
+sudo certbot -m $EMAIL \
+    --nginx \
+    --agree-tos \
+    --non-interactive \
+    --domains $URL_BACKEND,$URL_FRONTEND | tee -a "$LOG_FILE"
+
+# Finalizando instalação
+echo -e "${COLOR}Instalação finalizada com sucesso para a empresa: $NOME_EMPRESA!${RESET}" | tee -a "$LOG_FILE"
+
+# Exibindo resumo da instalação
+echo -e "${BOLD}Resumo da Instalação:${RESET}" | tee -a "$LOG_FILE"
+echo -e "${GREEN}---------------------------------------${RESET}" | tee -a "$LOG_FILE"
+echo -e "${BOLD}URL de Acesso (Frontend):${RESET} https://$URL_FRONTEND" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Nome da Instalação:${RESET} $NOME_EMPRESA" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Quantidade de Usuários Permitidos:${RESET} $USER_LIMIT" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Quantidade de Conexões Permitidas:${RESET} $CONNECTION_LIMIT" | tee -a "$LOG_FILE"
+echo -e "${BOLD}---------------------------------------${RESET}" | tee -a "$LOG_FILE"
+
+# Informações de Usuários
+echo -e "${BOLD}Usuário Padrão para Acesso${RESET}" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Usuário:${RESET} admin@pressticket.com.br" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Senha:${RESET} admin" | tee -a "$LOG_FILE"
+echo -e "${BOLD}---------------------------------------${RESET}" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Usuário Master para Acesso${RESET}" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Usuário:${RESET} masteradmin@pressticket.com.br" | tee -a "$LOG_FILE"
+echo -e "${BOLD}Senha:${RESET} masteradmin" | tee -a "$LOG_FILE"
+echo -e "${GREEN}---------------------------------------${RESET}" | tee -a "$LOG_FILE"
+
+# Mensagem final
+echo -e "${COLOR}Acesse o sistema e configure conforme necessário. Obrigado por utilizar o Press Ticket!${RESET}" | tee -a "$LOG_FILE"
