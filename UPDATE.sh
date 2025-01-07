5dce50dba51001be1cfc6e53ded5d125c05dca31
Robson Tenorio Henriques <robson.tenorio@gmail.com>
2025-01-07 11:25:42 -0300

    o

--- UPDATE.sh
+++ UPDATE.sh
@@ -148,20 +148,20 @@ fi
 sleep 2
 
 # Solicita confirmação do usuário para atualização do sistema operacional
-echo " "
-echo -e "${BOLD}${GREEN}Deseja atualizar os pacotes do sistema operacional antes de continuar? (s/n)${RESET}"
-read -r UPDATE_SYSTEM
+# echo " "
+# echo -e "${BOLD}${GREEN}Deseja atualizar os pacotes do sistema operacional antes de continuar? (s/n)${RESET}"
+# read -r UPDATE_SYSTEM
 
-# if [ -t 0 ]; then
-#     echo -e "${BOLD}${GREEN}Deseja atualizar os pacotes do sistema operacional antes de continuar? (s/n)${RESET}"
-#     read -r UPDATE_SYSTEM
-#     UPDATE_SYSTEM=${UPDATE_SYSTEM:-n} # Define 'n' como valor padrão se vazio
-# else
-#     # Abre um subshell para capturar entrada interativa
-#     UPDATE_SYSTEM=$(bash -c 'read -p "Deseja atualizar os pacotes do sistema operacional antes de continuar? (s/n): " REPLY; echo $REPLY' </dev/tty)
-#     UPDATE_SYSTEM=${UPDATE_SYSTEM:-n} # Define 'n' como valor padrão se vazio
-#     echo "DEBUG: Entrada capturada em modo não interativo: $UPDATE_SYSTEM"
-# fi
+if [ -t 0 ]; then
+    echo -e "${BOLD}${GREEN}Deseja atualizar os pacotes do sistema operacional antes de continuar? (s/n)${RESET}"
+    read -r UPDATE_SYSTEM
+    UPDATE_SYSTEM=${UPDATE_SYSTEM:-n} # Define 'n' como valor padrão se vazio
+else
+    # Abre um subshell para capturar entrada interativa
+    UPDATE_SYSTEM=$(bash -c 'read -p "Deseja atualizar os pacotes do sistema operacional antes de continuar? (s/n): " REPLY; echo $REPLY' </dev/tty)
+    UPDATE_SYSTEM=${UPDATE_SYSTEM:-n} # Define 'n' como valor padrão se vazio
+    echo "DEBUG: Entrada capturada em modo não interativo: $UPDATE_SYSTEM"
+fi
 
 if [[ "$UPDATE_SYSTEM" == "s" || "$UPDATE_SYSTEM" == "S" ]]; then
     # Comandos de atualização
