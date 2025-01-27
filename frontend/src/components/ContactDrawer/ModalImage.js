e1191ed718baa1c1bb33029ac5e1a151870c5535
Robson Tenorio <robson.tenorio@gmail.com>
2025-01-27 15:07:29 +0000

    Corrige problema de CORS com renderização direta de imagem

--- frontend/src/components/ContactDrawer/ModalImage.js
+++ frontend/src/components/ContactDrawer/ModalImage.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React	 from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 import ModalImage from "react-modal-image";
-import api from "../../services/api";
 
 const useStyles = makeStyles(theme => ({
 	messageMedia: {
@@ -16,30 +15,13 @@ const useStyles = makeStyles(theme => ({
  
 const ModalImageContatc = ({ imageUrl }) => {
 	const classes = useStyles();
-	const [fetching, setFetching] = useState(true);
-	const [blobUrl, setBlobUrl] = useState("");
-
-	useEffect(() => {
-		if (!imageUrl) return;
-		const fetchImage = async () => {
-			const { data, headers } = await api.get(imageUrl, {
-				responseType: "blob",
-			});
-			const url = window.URL.createObjectURL(
-				new Blob([data], { type: headers["content-type"] })
-			);
-			setBlobUrl(url);
-			setFetching(false);
-		};
-		fetchImage();
-	}, [imageUrl]);
-
+	
 	return (
 		<ModalImage
 			className={classes.messageMedia}
-			smallSrcSet={fetching ? imageUrl : blobUrl}
-			medium={fetching ? imageUrl : blobUrl}
-			large={fetching ? imageUrl : blobUrl}
+			smallSrcSet={imageUrl}
+			medium={imageUrl}
+			large={imageUrl}
 			showRotate="true"
 			alt="image"
 		/>
